ae60a527534915e5ca1ca8c0383398a0d3788401
César Barbosa <93480237+CesarBarbosa01@users.noreply.github.com>
2023-03-20 11:51:48 -0300

    ala

--- Node/node.js
+++ Node/node.js
@@ -1,3 +1,18 @@
 //"express"
+function removerDuplicados(lista) {
+    const valorUnicos = [];
+    const listaSemDuplicados =[];
 
-console.log("olá");
+    for (let i of lista) {
+        let valor = i;
+        if (!valorUnicos[valor]) {
+            valorUnicos[valor] = valor;
+            listaSemDuplicados.push(valor);
+        }
+    }
+    return listaSemDuplicados
+}
+
+let listaComDuplicados = [1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let listaSemDuplicados = removerDuplicados(listaComDuplicados)
+console.log(listaSemDuplicados)
