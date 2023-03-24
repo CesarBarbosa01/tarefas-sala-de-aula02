9f06459b0e77f0ba372103c495c1dc05c7e79024
César Barbosa <93480237+CesarBarbosa01@users.noreply.github.com>
2023-03-24 09:35:19 -0300

    refomulação

--- Node/S.js
+++
@@ -1,8 +0,0 @@
-let estado = ["Amapá", "São Paulo", "Rio de Janeiro", "Santa Catarina"]
-let resultado = estado.filter((valor) => {
-    if(valor.toLocaleLowerCase().charAt(0) === "s") {
-        return valor;
-    }
-})
-
-console.log(resultado)
