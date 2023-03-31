dca50feb4a6a573625098d8ac78ec0556c6fe663
César Barbosa <93480237+CesarBarbosa01@users.noreply.github.com>
2023-03-31 10:46:06 -0300

    sei la

--- index/index.js
+++ index/index.js
@@ -1,4 +1,21 @@
 const express = require("express")
 const app = express();
 
-app.listen()
+app.listen(3111, () => {
+    console.log("O servidor está rodando...")
+})
+
+app.use(express.json())
+
+
+let info = []
+// receber dados
+app.get("/meus-dados", (req, res) => {
+    res.json(info)
+})
+// enviar informações
+app.post("/meus-dados", (req, res) => {
+    const novodados = res.body
+})
+
+
