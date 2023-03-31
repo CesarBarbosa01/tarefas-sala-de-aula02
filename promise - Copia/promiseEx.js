d68756d963f506b4ea2ba8d1d3a135b9ed300b65
César Barbosa <93480237+CesarBarbosa01@users.noreply.github.com>
2023-03-31 09:04:56 -0300

    copia

---
+++ promise - Copia/promiseEx.js
@@ -0,0 +1,16 @@
+async function acharIdade(Idade) {
+        return await new Promise((resolve, reject) => {
+            const pessoas = [
+                {nome: "Gustavo", idade1: 14},
+                {nome: "Eduarda", idade1: 19},
+                {nome: "Ana", idade1: 39},
+            ]
+
+            if(Idade) {
+                
+            } 
+        })
+}
+
+acharIdade().then(resposta => console.log(resposta)).catch(error => console.error(error))
+acharIdade().then(resposta => console.log(resposta)).catch(error => console.error(error))
